24458bcaa1854b4d7d53d7ef27bbaaad6f1ea456
Teirza Tippets <[Teirza-Tippets]@users.noreply>
2025-05-17 11:52:23 -0500

    corrections

--- src/app/documents/document-list/document-list.component.ts
+++ src/app/documents/document-list/document-list.component.ts
@@ -12,10 +12,11 @@ import { Document } from '../document.model';
 })
 export class DocumentListComponent {
   documents: Document[] = [
-    new Document('1', 'Doc 1', 'Description 1', 'http://example.com/1', null),
-    new Document('2', 'Doc 2', 'Description 2', 'http://example.com/2', null),
-    new Document('3', 'Doc 3', 'Description 3', 'http://example.com/3', null),
-    new Document('4', 'Doc 4', 'Description 4', 'http://example.com/4', null)
+    new Document('1', 'Doc 1', ' Description 1', ' http://example.com/1', null),
+    new Document('2', 'Doc 2', ' Description 2', ' http://example.com/2', null),
+    new Document('3', 'Doc 3', ' Description 3', ' http://example.com/3', null),
+    new Document('4', 'Doc 4', ' Description 4', ' http://example.com/4', null),
+    new Document('5', 'Doc 5', ' Description 5', ' http://example.com/5', null)
   ];
 
   @Output() selectedDocumentEvent = new EventEmitter<Document>();
--- src/app/shared/dropdown.directive.ts
+++ src/app/shared/dropdown.directive.ts
@@ -6,7 +6,8 @@ import { Directive, HostListener, HostBinding } from '@angular/core';
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
 
-  @HostListener('click') toggleOpen() {
+  @HostListener('click', ['$event']) toggleOpen(event: Event) {
+    event.preventDefault();
     this.isOpen = !this.isOpen;
   }
 }
